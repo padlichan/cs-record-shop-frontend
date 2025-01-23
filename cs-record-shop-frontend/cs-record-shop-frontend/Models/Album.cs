1b4f17061a39bc07a664b5d0d144ac5e8a046e91
Lili <lili.berenyi@gmail.com>
2025-01-23 15:40:29 +0000

    Implement simple website with test data

---
+++ cs-record-shop-frontend/cs-record-shop-frontend/Models/Album.cs
@@ -0,0 +1,18 @@
+﻿namespace cs_record_shop_frontend.Models;
+public class Album
+{
+    public Album(int id, string title, string description, int year, string artistName)
+    {
+        Id = id;
+        Title = title;
+        Description = description;
+        Year = year;
+        ArtistName = artistName;
+    }
+
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Year { get; set; }
+    public string ArtistName { get; set; }
+}
